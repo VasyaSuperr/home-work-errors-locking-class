da93a7e548fd71d1a5a1208e850ffc77ef884203
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-06-06 16:06:45 +0300

    Task with * - closure

--- assets/js/closure.js
+++ assets/js/closure.js
@@ -12,3 +12,17 @@ const functionCloneInit = counter(5);
 console.log(functionCloneInit());
 console.log(functionCloneInit());
 console.log(functionCloneInit());
+
+function counter1(count, step) {
+  function initialisation() {
+    // debugger;
+    return (count += step);
+  }
+
+  return initialisation;
+}
+
+const functionCloneInit1 = counter1(1, 5);
+console.log(functionCloneInit1());
+console.log(functionCloneInit1());
+console.log(functionCloneInit1());
